28d92ba8d280a9246c25d184265e8fe9ce48f498
akhabr09-spec <akhabr09@github.com>
2026-08-19 11:51:23 +0200

    chameleon-paint-hide: cache-bust framework.js to force reload patched version

--- chameleon-paint-hide/lib/runtimeData.js
+++ chameleon-paint-hide/lib/runtimeData.js
@@ -12,10 +12,10 @@ const runtimeData = (function () {
         // File references.
         buildURL: "bin",
         loaderURL: "bin/Chameleon_v3_Web_YandexGames.loader.js",
-        dataURL: "bin/Chameleon_v3_Web_YandexGames.data",
-        frameworkURL: "bin/Chameleon_v3_Web_YandexGames.framework.js",
+        dataURL: "https://akhabr09-spec.github.io/eclipse-games/chameleon-paint-hide/bin/Chameleon_v3_Web_YandexGames.data",
+        frameworkURL: "bin/Chameleon_v3_Web_YandexGames.framework.js?v=2",
         workerURL: "",
-        codeURL: "bin/Chameleon_v3_Web_YandexGames.wasm",
+        codeURL: "https://akhabr09-spec.github.io/eclipse-games/chameleon-paint-hide/bin/Chameleon_v3_Web_YandexGames.wasm",
         symbolsURL: "",
         streamingURL: "streaming",
 
